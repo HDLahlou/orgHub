df7bdad24f4103b3e765cda0e6f6ab1dedccb703
locb65 <locb65@gmail.com>
2024-05-30 21:56:05 -0400

    added simple login form

--- orghub/src/components/LoginForm.jsx
+++ orghub/src/components/LoginForm.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from './Button'
 
 export const LoginForm = () => {
   return (
-    <div>LoginForm</div>
+    <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <Button btnName={"Login"} />
+        <Button btnName={"Sign Up"} />
+    </form>
   )
 }
