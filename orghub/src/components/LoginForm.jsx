24ce0e9aa4ef03f05c3e546c16e9871ed277c6d7
locb65 <locb65@gmail.com>
2024-05-30 21:59:59 -0400

    added comments for what to do next

--- orghub/src/components/LoginForm.jsx
+++ orghub/src/components/LoginForm.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Button } from './Button'
 
 export const LoginForm = () => {
+    // need to implement state for passing data to backend
+    // need to implement clicking handlers for btns and navigation
+    // need to generate basic template for SignupForm
   return (
     <form>
         <input type="text" placeholder="Username" />
