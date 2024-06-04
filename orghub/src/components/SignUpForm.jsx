eddba667e647f21072bb4237a2097b2960243904
locb65 <locb65@gmail.com>
2024-06-04 19:51:59 -0400

    moved submit btn on signup and logic to the form component

--- orghub/src/components/SignUpForm.jsx
+++ orghub/src/components/SignUpForm.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-// import { Button } from './Button'
+import { useNavigate } from 'react-router-dom'
+import { Button } from './Button'
 import { InputLabel } from './InputLabel'
 
 export const SignUpForm = () => {
+    const navigate = useNavigate();
+    function handleSubmitButton (e) {
+        e.preventDefault();
+        navigate("/login");
+        // need to add state and fetch to backend using useState and useEffect(fetch request)
+    }
   return (
     <div>
         <form>
@@ -30,7 +37,7 @@ export const SignUpForm = () => {
                 <InputLabel labelName = {"Confirm Password"}  />
                 <input required type="password" placeholder="Confirm Password" />
             </div>
-            {/* <Button btnName={"Submit"}/> */}
+            <Button btnName={"Submit"} onClick={handleSubmitButton}/>
         </form>
     </div>
   )
--- orghub/src/pages/SignUp.jsx
+++ orghub/src/pages/SignUp.jsx
@@ -1,14 +1,20 @@
 import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { Button } from '../components/Button';
+// import { useNavigate } from 'react-router-dom';
+// import { Button } from '../components/Button';
 import { SignUpForm } from '../components/SignUpForm';
 
 export const SignUp = () => {
+    // const navigate = useNavigate();
+    // function handleSubmitButton (e) {
+    //     e.preventDefault();
+    //     navigate("/login");
+    //     // need to add state and fetch to backend using useState and useEffect(fetch request)
+    // }
   return (
     <div>
         <h2>SignUp</h2>
         <SignUpForm />
-        <Button btnName={"Submit"}/>
+        {/* <Button btnName={"Submit"} onClick={handleSubmitButton}/> */}
     </div>
   )
 }
